7021b1f5249db15aedb58bceb144bc4fbbd691e3
Victor Arango <victorarangoquiroz@gmail.com>
2026-03-09 11:10:49 -0500

    add name route to pages

--- Frontend/src/components/header/header.ts
+++ Frontend/src/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../auth/services/auth.service';
+import { NameRoute } from "../name-route/name-route";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NameRoute],
   templateUrl: './header.html',
 })
 export class Header {
